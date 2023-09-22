87d6c61a082ba192a1ac13e2af5f6622ed3c5938
Sara <saracoppola996@gmail.com>
2023-09-22 11:31:34 +0200

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "admin", "Reggina1914admin");
+            Statement statement = conn.createStatement();
+
+            statement.executeUpdate("ALTER TABLE students ADD country VARCHAR(30);");
+            statement.executeUpdate("UPDATE student SET country = 'Italy' WHERE student_id IN (1,2);");
+            statement.executeUpdate("UPDATE student SET country = 'Germany' WHERE student_id IN (3,4);");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
