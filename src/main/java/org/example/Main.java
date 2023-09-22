3ce0c14773e28bf8c69ce628dd2fa9818da16e0c
Sara <saracoppola996@gmail.com>
2023-09-22 11:33:20 +0200

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "admin", "Reggina1914admin");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "admin", "admin");
             Statement statement = conn.createStatement();
 
             statement.executeUpdate("ALTER TABLE students ADD country VARCHAR(30);");
